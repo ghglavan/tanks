9fd027210da74e4037ae7057c1dc3455c476a11f
x2009 <george.glavan27@gmail.com>
2018-03-27 16:16:38 +0300

    asdsadasdas

--- gg_proto/gg_p.py
+++ gg_proto/gg_p.py
@@ -12,5 +12,9 @@ class MessageType(IntEnum):
     UsersUpdate      = 4
     UserDisconnected = 5
     UserKilled       = 6
-    UsersPositions    = 7
+    UsersPositions   = 7
     ServerApprove    = 8
+    ConnectTo        = 9
+    ServerResetUsers = 10
+    RequestPosition  = 11
+    RequestUsers     = 12
